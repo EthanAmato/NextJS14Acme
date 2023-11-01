8fb5edf2949ffde9dfd1a44532d49106d766e563
EthanAmato <ethannamato@gmail.com>
2023-11-01 11:01:47 -0400

    added invoice page, query param integration, and debounced server queries

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,24 +1,20 @@
-import { Card } from "@/app/ui/dashboard/cards";
-import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import CardWrapper from "@/app/ui/dashboard/cards";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import { lusitana } from "@/app/ui/fonts";
-import { Suspense } from "react";
-import { fetchLatestInvoices, fetchCardData } from "../../lib/data";
 import {
-  RevenueChartSkeleton,
+  CardsSkeleton,
   LatestInvoicesSkeleton,
+  RevenueChartSkeleton
 } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { fetchLatestInvoices } from "../../lib/data";
+
 export default async function Page() {
   // This is a waterfall pattern and is viable when you might want to satisfy some condition
   // before making a subsequent request
   // However it may unintentionally slow down your app. You can resolve waterfalls by doing Promise.all
   const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
 
   return (
     <main>
@@ -26,14 +22,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardsSkeleton/>}>
+          <CardWrapper/>
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,30 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter(); //
+
+  // Callback function that modifies the URL search parameters for easy refetching of data based on a term
+  // URL is updated without reloading the page
+  // useDebouncedCallback will wait for a specified amount of time before running the callback
+  // this will allow us to not bog down our server with incessant redundant calls
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+    // Will get search params in URL like ?page=4&limit=100
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -10,6 +32,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        onChange={(e) => handleSearch(e.target.value)}
+        // Set the default value inside of the input to the searchparam with key 'query' if it exists
+        defaultValue={searchParams.get("query")?.toString()}
         placeholder={placeholder}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
